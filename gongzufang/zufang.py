375e1458a2912d9859868261b275743fda7e71e1
007root <007root@users.noreply.github.com>
2020-03-24 08:37:14 +0800

    Add server酱 to send message

--- gongzufang/zufang.py
+++ gongzufang/zufang.py
@@ -1,6 +1,8 @@
+#-*- coding:utf8 -*-
 from pyquery import PyQuery as pq
 import urllib
 from mail import send_mail
+import requests
 pre_url = "http://www.bphc.com.cn"
 gao_hash = "/tmp/.html"
 yewu_url = "http://www.bphc.com.cn/front/noroomstaff/planLists"
@@ -29,6 +31,7 @@ g_new = str(hash(verify))
 
 if g_new != g_old or g_old == None:
     send_mail(mess, pre_url + url)
+    requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[%s](%s)" %(mess.encode('utf8'), pre_url + url))
     g_f = file(gao_hash, 'w')
     g_f.write(g_new)
     g_f.close()
@@ -40,7 +43,7 @@ try:
     y_old = y.read()
     y.close()
 except:
-    y_old = None     
+    y_old = None
 
 y_page = urllib.urlopen(yewu_url).read()
 y_new = str(hash(y_page))
@@ -48,6 +51,7 @@ y_new = str(hash(y_page))
 
 if y_new != y_old or y_old == None:
     send_mail('ZZZFFF', yewu_url)
+    requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[业务](%s)" % yewu_url)
     y_f = file(yewu_hash, 'w')
     y_f.write(y_new)
     y_f.close()
