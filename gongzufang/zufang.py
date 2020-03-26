8d802c6311380fc696c25f967a8d177e6a2abf6c
007root <007root@users.noreply.github.com>
2020-03-26 13:59:36 +0800

    网站频繁500

--- gongzufang/zufang.py
+++ gongzufang/zufang.py
@@ -3,6 +3,8 @@ from pyquery import PyQuery as pq
 import urllib
 from mail import send_mail
 import requests
+
+
 pre_url = "http://www.bphc.com.cn"
 gao_hash = "/tmp/.html"
 yewu_url = "http://www.bphc.com.cn/front/noroomstaff/planLists"
@@ -17,24 +19,25 @@ try:
 except:
     g_old = None
 
-g_page = urllib.urlopen("%s/article/list/b02e7e29e33642f789e4d1e41db08b7d.html" % pre_url).read()
-d = pq(g_page)
-div = d('.publicity')
-ul = div('ul')
-a = ul('a')[0]
-url = a.get('href')
-h = ul('h2')[0]
-mess = h.text
-verify = url + mess
-g_new = str(hash(verify))
+g_ret = urllib.urlopen("%s/article/list/b02e7e29e33642f789e4d1e41db08b7d.html" % pre_url)
+if ret.getcode() == 200:
+    g_page = g_ret.read()
+    d = pq(g_page)
+    div = d('.publicity')
+    ul = div('ul')
+    a = ul('a')[0]
+    url = a.get('href')
+    h = ul('h2')[0]
+    mess = h.text
+    verify = url + mess
+    g_new = str(hash(verify))
 
-
-if g_new != g_old or g_old == None:
-    send_mail(mess, pre_url + url)
-    requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[%s](%s)" %(mess.encode('utf8'), pre_url + url))
-    g_f = file(gao_hash, 'w')
-    g_f.write(g_new)
-    g_f.close()
+    if g_new != g_old or g_old == None:
+        send_mail(mess, pre_url + url)
+        requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[%s](%s)" %(mess.encode('utf8'), pre_url + url))
+        g_f = file(gao_hash, 'w')
+        g_f.write(g_new)
+        g_f.close()
 
 
 # capture yewu url
@@ -45,13 +48,14 @@ try:
 except:
     y_old = None
 
-y_page = urllib.urlopen(yewu_url).read()
-y_new = str(hash(y_page))
+y_ret = urllib.urlopen(yewu_url)
+if ret.getcode() == 200:
+    y_page = y_ret.read()
+    y_new = str(hash(y_page))
 
-
-if y_new != y_old or y_old == None:
-    send_mail('ZZZFFF', yewu_url)
-    requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[业务](%s)" % yewu_url)
-    y_f = file(yewu_hash, 'w')
-    y_f.write(y_new)
-    y_f.close()
+    if y_new != y_old or y_old == None:
+        send_mail('ZZZFFF', yewu_url)
+        requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[业务](%s)" % yewu_url)
+        y_f = file(yewu_hash, 'w')
+        y_f.write(y_new)
+        y_f.close()
