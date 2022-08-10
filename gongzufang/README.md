0cfd561370ed504a61e41e09e92d84f92cce7b32
007root <007root>
2022-08-10 15:35:46 +0800

    升级到python3; 移除邮件发送和server酱; 使用企业微信发送消息

--- gongzufang/README.md
+++ gongzufang/README.md
@@ -1,6 +1,7 @@
 用于抓取公租房公告信息，及时通知自己能尽快办理相关租房业务；致北漂！！！
 
 1. 依赖pyquery
-      pip install pyquery
+      pip3 install pyquery
+      pip3 install -e git+https://github.com/007root/weixin_api.git#egg=weixin_api
 2. 添加定时任务
-    */5 * * * * /usr/bin/python /home/zufang/zufang.py
+    */5 * * * * /usr/bin/python3 /home/zufang/zufang.py
--- gongzufang/zufang.py
+++ gongzufang/zufang.py
@@ -1,9 +1,10 @@
 #-*- coding:utf8 -*-
+# python3
+
 from pyquery import PyQuery as pq
 import urllib
-from mail import send_mail
-import requests
-
+from weixin_api.ops import *
+import hashlib
 
 pre_url = "http://www.bphc.com.cn"
 gao_hash = "/tmp/.html"
@@ -13,13 +14,13 @@ yewu_hash = "/tmp/.yewu"
 
 # capture gonggao url
 try:
-    g_f = file(gao_hash, 'r')
+    g_f = open(gao_hash, 'r')
     g_old = g_f.read()
     g_f.close()
 except:
     g_old = None
 
-g_ret = urllib.urlopen("%s/article/list/b02e7e29e33642f789e4d1e41db08b7d.html" % pre_url)
+g_ret = urllib.request.urlopen("%s/article/list/b02e7e29e33642f789e4d1e41db08b7d.html" % pre_url)
 if g_ret.getcode() == 200:
     g_page = g_ret.read()
     d = pq(g_page)
@@ -30,32 +31,37 @@ if g_ret.getcode() == 200:
     h = ul('h2')[0]
     mess = h.text
     verify = url + mess
-    g_new = str(hash(verify))
+    g_new = hashlib.md5(verify.encode('utf8')).hexdigest()
 
     if g_new != g_old or g_old == None:
-        send_mail(mess, pre_url + url)
-        requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[%s](%s)" %(mess.encode('utf8'), pre_url + url))
-        g_f = file(gao_hash, 'w')
+        send_text(agentid, mess + '\n' + pre_url + url)
+        g_f = open(gao_hash, 'w')
         g_f.write(g_new)
         g_f.close()
 
 
 # capture yewu url
 try:
-    y = file(yewu_hash , 'r')
+    y = open(yewu_hash , 'r')
     y_old = y.read()
     y.close()
 except:
     y_old = None
 
-y_ret = urllib.urlopen(yewu_url)
+y_ret = urllib.request.urlopen(yewu_url)
 if y_ret.getcode() == 200:
     y_page = y_ret.read()
-    y_new = str(hash(y_page))
+    d = pq(y_page)
+    div = d('.planContent')
+    ul = div('h2')
+    h = ul('h2')[0]
+    mess = h.text
+    y_new = hashlib.md5(mess.encode('utf8')).hexdigest()
 
     if y_new != y_old or y_old == None:
-        send_mail('ZZZFFF', yewu_url)
-        requests.get("https://sc.ftqq.com/server酱密钥?text=公租房&desp=[业务](%s)" % yewu_url)
-        y_f = file(yewu_hash, 'w')
+        send_text(agentid, 'ZZZFFF登记\n' + yewu_url)
+        y_f = open(yewu_hash, 'w')
         y_f.write(y_new)
         y_f.close()
+
+
